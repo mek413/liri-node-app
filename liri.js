fc66af0b07ce7d8b411522f617c6e39c3ddf1379
Marcus Kelly <mek413@gmail.com>
2019-11-27 18:18:29 -0600

    added bands in town api to liri app

--- liri.js
+++ liri.js
@@ -1,3 +1,46 @@
 require("dotenv").config();
+var axios = require("axios");
+var moment = require('moment');
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+
 var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+var query = process.argv;
+
+if (command === "concert-this") {
+    var band = "";
+    for (var i = 3; i < query.length; i++) {
+     band += query[i];
+    }
+      
+    axios.get("https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp").then(
+        function(response){
+            console.log("----- Events -----")
+
+            for (var i = 0;i<response.data.length;i++) {
+                console.log("")
+                console.log("Venue: " + response.data[i].venue.name);
+                console.log("Location: " + response.data[i].venue.city + ", " +
+                response.data[i].venue.region + " " + response.data[i].venue.country);
+                console.log("Date of Event: " + moment(response.data[i].datetime).format("MMMM Do YYYY"));
+                console.log("")
+                console.log("=======================================================")
+            }
+        }
+    ).catch(err => {
+        console.log(err)
+    })
+}
+else if (command === "spotify-this-song") {
+    console.log("spotify API");
+}
+else if (command === "movie-this") {
+    console.log("omdb API");
+}
+else if (command === "do-what-it-says") {
+    console.log("ok man don't be so pushy");
+}else{
+    console.log("error command not found")
+}
